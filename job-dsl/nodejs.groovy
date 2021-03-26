53ce465e315e3a784cfd992ced36bb1cec8669b3
Aldred Benedict <45155724+aldredb@users.noreply.github.com>
2021-03-26 16:43:00 +0800

    Update nodejs.groovy

--- job-dsl/nodejs.groovy
+++ job-dsl/nodejs.groovy
@@ -1,15 +1,15 @@
 job('NodeJS example') {
     scm {
-        git('git://github.com/wardviaene/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
-            node / gitConfigName('DSL User')
-            node / gitConfigEmail('jenkins-dsl@newtech.academy')
+        git('git://github.com:aldredb/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
+            node / gitConfigName('Aldred')
+            node / gitConfigEmail('aldred@mailinator.com')
         }
     }
     triggers {
         scm('H/5 * * * *')
     }
     wrappers {
-        nodejs('nodejs') // this is the name of the NodeJS installation in 
+        nodejs('NodeJS-12') // this is the name of the NodeJS installation in 
                          // Manage Jenkins -> Configure Tools -> NodeJS Installations -> Name
     }
     steps {
